82b201fb08ed16f1d096b0f5769e75e063ff523d
Anatoli Miske <hello@anatolimiske.de>
2021-06-03 21:17:18 +0200

    add playRound with all possible outcomes

--- game.js
+++ game.js
@@ -4,5 +4,36 @@ function computerPlay() {
     return options[Math.floor(Math.random() * options.length)]
 }
 
+function playRound(playerSelection, computerSelection) {
+    let player = playerSelection.toLowerCase();
+    let winMsg = "Great! You win this round!";
+    let loseMsg = "Oh no! You lose this round!";
+
+    if (player == computerSelection) {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+        It's a tie!`)
+    } else if (player == "rock" && computerSelection == "scissors") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+        ${winMsg}`)
+    } else if (player == "rock" && computerSelection == "paper") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+                ${loseMsg}`)
+    } else if (player == "paper" && computerSelection == "scissors") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+                ${loseMsg}`)
+    } else if (player == "paper" && computerSelection == "rock") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+                ${winMsg}`)
+    } else if (player == "scissors" && computerSelection == "paper") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+                ${winMsg}`)
+    } else if (player == "scissors" && computerSelection == "rock") {
+        console.log(`You: ${playerSelection} | Computer: ${computerSelection}
+                ${loseMsg}`)
+    }
+}
+
 let computerSelection = computerPlay();
-let playerSelection = prompt("Please type one of the following: Rock, Paper or Scissors")
+let playerSelection = "rock"
+
+console.log(playRound(playerSelection, computerSelection));
