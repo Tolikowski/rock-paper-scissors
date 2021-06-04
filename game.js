f046e4da44e07bb5f4d82ac64cfacd0867f5fc43
Anatoli Miske <hello@anatolimiske.de>
2021-06-04 22:38:25 +0200

    add showMsg and showStatus

--- game.js
+++ game.js
@@ -16,41 +16,63 @@ function playRound(playerSelection, computerSelection) {
     let loseMsg = "Oh no! You lose this round!";
     let status = `You: ${playerSelection} | Computer: ${computerSelection}`
 
+    function showStatus() {
+        document.getElementById("status").innerHTML = status;
+    }
+
+    function updateMsg(msg) {
+        document.getElementById("msg").innerHTML = msg;
+    }
+
     if (player == computerSelection) {
         console.log("It's a tie!")
-        console.log(`You: ${playerSelection} | Computer: ${computerSelection}`)
+        console.log(status)
+        showStatus()
+        updateMsg("It's a tie!")
     } else if (player == "rock" && computerSelection == "scissors") {
         console.log(winMsg)
         console.log(status)
+        showStatus()
+        updateMsg(winMsg)
         playerScore++
     } else if (player == "rock" && computerSelection == "paper") {
         console.log(loseMsg)
         console.log(status)
+        showStatus()
+        updateMsg(loseMsg)
         computerScore++
     } else if (player == "paper" && computerSelection == "scissors") {
         console.log(loseMsg)
         console.log(status)
+        showStatus()
+        updateMsg(loseMsg)
         computerScore++
     } else if (player == "paper" && computerSelection == "rock") {
         console.log(winMsg)
         console.log(status)
+        showStatus()
+        updateMsg(winMsg)
         playerScore++
     } else if (player == "scissors" && computerSelection == "paper") {
         console.log(winMsg)
         console.log(status)
+        showStatus()
+        updateMsg(winMsg)
         playerScore++
     } else if (player == "scissors" && computerSelection == "rock") {
         console.log(loseMsg)
         console.log(status)
+        showStatus()
+        updateMsg(loseMsg)
         computerScore++
     } else {
         console.log("Invalid input!")
     }
     document.getElementById("yourScore").innerHTML = `You: ${playerScore}`;
     document.getElementById("compScore").innerHTML = `Computer: ${computerScore}`;
-    console.log(`Current Score: 
-    You ${playerScore}
-    Computer ${computerScore}`)
+    //console.log(`Current Score: 
+    //You ${playerScore}
+    //Computer ${computerScore}`)
 }
 
 let playerSelection
